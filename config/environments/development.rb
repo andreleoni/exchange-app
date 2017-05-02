763d9283b207566564ce748c5761facc614a1fcc
André Luiz Leoni <andreluizleoni@gmail.com>
2017-05-01 23:54:00 -0300

    corrigido bugs e adicionado better_errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #BetterErrors on Docker
+  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
