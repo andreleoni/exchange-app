43fe086b5472a7512da59efddf8799bf4b84836a
André Luiz Leoni <andreluizleoni@gmail.com>
2017-05-03 22:19:25 -0300

    ajustes testes

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'Exchange Currency Process', :type => :feature, :js => true do
+RSpec.describe 'Exchange Currency Process', :type => :feature do
   it 'exchange value' do
-    # visit '/'
-    #
-    # within('#exchange_form') do
-    #   select('EUR', from: 'currency')
-    #   select('USD', from: 'currency_destination')
-    #   fill_in 'quantity', with: '10'
-    # end
+    visit '/'
+
+    within('#exchange_form') do
+      select('EUR', from: 'currency')
+      select('USD', from: 'currency_destination')
+      fill_in 'quantity', with: '10'
+    end
 
     # page.find("body").click
     # expect(page).to have_content('value')
