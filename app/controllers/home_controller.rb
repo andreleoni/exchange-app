1cb823cd10cb2339bd10a05c1a90d99abeb20245
André Luiz Leoni <andreluizleoni@gmail.com>
2018-03-01 19:18:23 -0300

    xvc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,8 @@ class HomeController < ApplicationController
   	currency_value = Currency.exchange(params[:currency], params[:currency_destination], params[:quantity]).to_f
   	render json: { 'value': currency_value }
   end
+
+  def novo_teste
+    "asdadsa"
+  end
 end
